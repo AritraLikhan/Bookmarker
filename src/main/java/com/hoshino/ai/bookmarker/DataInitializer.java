3482a556d877fecbd4869b91d9687a7c745c462e
AritraLikhan <aritralikhan2001@gmail.com>
2024-10-10 10:54:46 +0600

    Flyway migration schema added

--- src/main/java/com/hoshino/ai/bookmarker/DataInitializer.java
+++ src/main/java/com/hoshino/ai/bookmarker/DataInitializer.java
@@ -1,31 +1,31 @@
-package com.hoshino.ai.bookmarker;
-
-import com.hoshino.ai.bookmarker.domain.Bookmark;
-import com.hoshino.ai.bookmarker.domain.BookmarkRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
-
-import java.time.Instant;
-
-@Component
-@RequiredArgsConstructor
-public class DataInitializer implements CommandLineRunner {
-    private final BookmarkRepository repository;
-
-    @Override
-    public void run(String... args)
-        throws Exception{
-        repository.save(new Bookmark(null,"Sivalabs","https://www.google.com", Instant.now()));
-        repository.save(new Bookmark(null,"Alibaba","https://www.alibaba.com", Instant.now()));
-        repository.save(new Bookmark(null,"Sivalabs","https://www.google.com", Instant.now()));
-        repository.save(new Bookmark(null,"Alibaba","https://www.alibaba.com", Instant.now()));
-        repository.save(new Bookmark(null,"Sivalabs","https://www.google.com", Instant.now()));
-        repository.save(new Bookmark(null,"Alibaba","https://www.alibaba.com", Instant.now()));
-        repository.save(new Bookmark(null,"Sivalabs","https://www.google.com", Instant.now()));
-        repository.save(new Bookmark(null,"Alibaba","https://www.alibaba.com", Instant.now()));
-
-
-    }
-
-}
+//package com.hoshino.ai.bookmarker;
+//
+//import com.hoshino.ai.bookmarker.domain.Bookmark;
+//import com.hoshino.ai.bookmarker.domain.BookmarkRepository;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.stereotype.Component;
+//
+//import java.time.Instant;
+//
+//@Component
+//@RequiredArgsConstructor
+//public class DataInitializer implements CommandLineRunner {
+//    private final BookmarkRepository repository;
+//
+//    @Override
+//    public void run(String... args)
+//        throws Exception{
+//        repository.save(new Bookmark(null,"Sivalabs","https://www.google.com", Instant.now()));
+//        repository.save(new Bookmark(null,"Alibaba","https://www.alibaba.com", Instant.now()));
+//        repository.save(new Bookmark(null,"Sivalabs","https://www.google.com", Instant.now()));
+//        repository.save(new Bookmark(null,"Alibaba","https://www.alibaba.com", Instant.now()));
+//        repository.save(new Bookmark(null,"Sivalabs","https://www.google.com", Instant.now()));
+//        repository.save(new Bookmark(null,"Alibaba","https://www.alibaba.com", Instant.now()));
+//        repository.save(new Bookmark(null,"Sivalabs","https://www.google.com", Instant.now()));
+//        repository.save(new Bookmark(null,"Alibaba","https://www.alibaba.com", Instant.now()));
+//
+//
+//    }
+//
+//}
