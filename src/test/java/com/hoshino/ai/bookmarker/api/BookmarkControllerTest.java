1fd66b3a288bca8aebf0fb04548cdb0ed1ceaf99
AritraLikhan <aritralikhan2001@gmail.com>
2024-10-10 13:50:10 +0600

    Integration testing done using Testcontainer dependency

---
+++ src/test/java/com/hoshino/ai/bookmarker/api/BookmarkControllerTest.java
@@ -0,0 +1,77 @@
+package com.hoshino.ai.bookmarker.api;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import com.hoshino.ai.bookmarker.domain.Bookmark;
+import com.hoshino.ai.bookmarker.domain.BookmarkRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.testcontainers.shaded.org.hamcrest.CoreMatchers;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+class BookmarkControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    BookmarkRepository bookmarkRepository;
+
+    private List<Bookmark> bookmarks;
+
+    @BeforeEach
+    void setUp() {
+        bookmarkRepository.deleteAllInBatch();
+        bookmarks = new ArrayList<>();
+
+        bookmarks.add(new Bookmark(null, "SivaLabs", "https://sivalabs.in", Instant.now()));
+        bookmarks.add(new Bookmark(null, "SpringBlog", "https://spring.io/blog", Instant.now()));
+        bookmarks.add(new Bookmark(null, "Quarkus", "https://quarkus.io/", Instant.now()));
+        bookmarks.add(new Bookmark(null, "Micronaut", "https://micronaut.io/", Instant.now()));
+        bookmarks.add(new Bookmark(null, "JOOQ", "https://www.jooq.org/", Instant.now()));
+        bookmarks.add(new Bookmark(null, "VladMihalcea", "https://vladmihalcea.com", Instant.now()));
+
+
+        bookmarkRepository.saveAll(bookmarks);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "1,6,2,1,true,false,true,false",
+            "2,6,2,2,false,true,false,true"
+    })
+    void shouldGetBookmarks(int pageNo, int totalElements, int totalPages, int currentPage,
+                            boolean isFirst, boolean isLast,
+                            boolean hasNext, boolean hasPrevious) throws Exception {
+        mvc.perform(get("/api/bookmarks?page=" + pageNo))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.totalElements", equalTo(totalElements)))
+                .andExpect(jsonPath("$.totalPages", equalTo(totalPages)))
+                .andExpect(jsonPath("$.currentPage", equalTo(currentPage)))
+                .andExpect(jsonPath("$.isFirst", equalTo(isFirst)))
+                .andExpect(jsonPath("$.isLast", equalTo(isLast)))
+                .andExpect(jsonPath("$.hasNext", equalTo(hasNext)))
+                .andExpect(jsonPath("$.hasPrevious", equalTo(hasPrevious)));
+    }
+
+}
